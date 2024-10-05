0a331a69b4a3b554dc17ec27b70a75e93f6ac55c
Qkaii12 <khaigamingqce@gmail.com>
2024-10-05 21:21:10 +0700

    Spawn Fruits

--- Assets/Prezip/FruitNinja/Script/Fruits.cs
+++ Assets/Prezip/FruitNinja/Script/Fruits.cs
@@ -6,24 +6,27 @@ public class Fruits : MonoBehaviour
 {
     public GameObject slicedFruit;
     public GameObject fruitJuice;
+    public float rotationForce = 200;
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        transform.Rotate(Vector2.right * Time.deltaTime * rotationForce);
     }
     private void InstantiateSlicedFruit()
     {
         GameObject instantiatedFruit = Instantiate(slicedFruit, transform.position, transform.rotation);
-        GameObject instantiatedJuice = Instantiate(fruitJuice, new Vector3(transform.position.x, transform.position.y, 0), fruitJuice.transform.rotation);
+        GameObject instantiatedJuice = Instantiate(fruitJuice, /*new Vector3(transform.position.x, transform.position.y, 0)*/transform.position, fruitJuice.transform.rotation);
         Rigidbody[] slicedRb = instantiatedFruit.transform.GetComponentsInChildren<Rigidbody>();
 
         foreach (Rigidbody rb in slicedRb)
         {
             rb.AddExplosionForce(130f, transform.position, 10);
+            rb.velocity = rb.velocity * 1.2f;
         }
 
         Destroy(instantiatedFruit, 5);
