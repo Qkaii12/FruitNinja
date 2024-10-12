27ffc7eb65705fdf19355281ec1a27af8bcde8af
Qkaii12 <khaigamingqce@gmail.com>
2024-10-12 18:51:41 +0700

    Blade can slice Bomb

--- Assets/Prezip/FruitNinja/Script/BombScript.cs
+++ Assets/Prezip/FruitNinja/Script/BombScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BombScript : MonoBehaviour
 {
     private float rotationForce = 200;
+    public ParticleSystem explosionParticle;
     void Start()
     {
         transform.rotation = Random.rotation;
@@ -13,5 +14,13 @@ public class BombScript : MonoBehaviour
     void Update()
     {
         transform.Rotate(Vector2.right * Time.deltaTime * rotationForce);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Blade")
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
     }
 }
